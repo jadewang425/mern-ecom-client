b5e83a1ca489c196daf99d5a14e7e5de4cae7a56
jadewang425 <jadewang425@gmail.com>
2024-05-16 22:10:16 -0400

    added drop down list for category selection on production creation page

--- src/pages/admin/Product.js
+++ src/pages/admin/Product.js
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react"
 import { useAuth } from "../../context/auth"
 import Jumbotron from "../../components/cards/jumbotron"
 import AdminNavbar from "../../components/AdminNavbar"
+import axios from "axios"
+import { Select } from "antd"
 
 export default function AdminProduct() {
     // context
     const [ auth, setAuth ] = useAuth()
+
+    // state
+    const [ categories, setCategories ] = useState([])
+    const [ photo, setPhoto ] = useState('')
+    const [ name, setName ] = useState('')
+    const [ description, setDescription ] = useState('')
+    const [ price, setPrice ] = useState('')
+    const [ category, setCategory ] = useState('')
+    const [ shipping, setShipping ] = useState('')
+    const [ quantity, setQuantity ] = useState('')
+
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    const loadCategories = async () => {
+        try {
+            const { data } = await axios.get('/categories')
+            setCategories(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const onChange = (value) => {
+        console.log(`selected ${value}`)
+    }
+    const onSearch = (value) => {
+        console.log('search:', value)
+    }
+
+    const filterOption = (input, option) =>
+        (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
 
     return (
         <>
@@ -15,8 +51,16 @@ export default function AdminProduct() {
                         <AdminNavbar />
                     </div>
                     <div className="col-md-9">
-                        <div className="p-3 mt-2 mb-2 bg-light h4">Manage Products</div>
-                        <p>Create product form</p>
+                        <div className="p-3 mt-2 mb-2 bg-light h4">Create Products</div>
+                        <Select
+                            showSearch
+                            placeholder='Select a category'
+                            optionFilterProp="children"
+                            onChange={onChange}
+                            onSearch={onSearch}
+                            filterOption={filterOption}
+                            options={categories?.map(c => ({value: c.name, label: c.name}))}
+                        />
                     </div>
                 </div>
             </div>
