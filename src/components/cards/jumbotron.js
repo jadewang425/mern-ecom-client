6e99b0e7b9370246cb24523ccf15d554bbe0d9bd
jadewang425 <jadewang425@gmail.com>
2024-05-29 17:23:52 -0400

    added gradient effect to jumbotron

--- src/components/cards/jumbotron.js
+++ src/components/cards/jumbotron.js
@@ -2,9 +2,9 @@ export default function Jumbotron(props) {
     // set subtitle to default
     const { title, subTitle="Welcome to React E-commerce" } = props
     return (
-        <div className="container-fluid bg-secondary">
+        <div className="container-fluid jumbotron">
             <div className="row">
-                <div className="col text-center p-5 bg-light">
+                <div className="col text-center p-5">
                     <h1>{title}</h1>
                     <p className="lead">{subTitle}</p>
                 </div>
