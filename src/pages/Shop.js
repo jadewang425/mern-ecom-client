7da8fcac72ba269ac5e05e805f8e7d558fc155ed
jadewang425 <jadewang425@gmail.com>
2024-05-30 21:38:02 -0400

    added Shop page and product cards

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,3 +1,61 @@
+import { useEffect, useState } from "react"
+import Jumbotron from "../components/cards/jumbotron"
+import axios from "axios"
+import ProductCard from "../components/cards/ProductCard"
+
 export default function Shop() {
-    return <h1>Shop Page</h1>
+    const [ categories, setCategories ] = useState([])
+    const [ products, setProducts ] = useState([])
+
+    useEffect(() => {
+        loadProducts()
+    }, [])
+
+    const loadProducts = async () => {
+        try {
+            const { data } = await axios.get('/products')
+            setProducts(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    const loadCategories = async () => {
+        try {
+            const { data } = await axios.get('/categories')
+            setCategories(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    console.log('categories', categories)
+
+    return (
+        <>
+            <Jumbotron title="Shop" subTitle="" />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-3">
+                        sidebar
+                    </div>
+
+                    <div className="col-md-9">
+                        <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">{products?.length} Products</h2>
+                        <div className="row">
+                            {products?.map((p) => (
+                                <div className="col-md-4" key={p._id}>
+                                    <ProductCard p={p} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
